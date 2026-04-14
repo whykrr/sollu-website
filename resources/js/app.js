8f0b4a36aec79840253115dda3c6a6ffd08c36ab
Wahyu Kristiawan <whykris97@gmail.com>
2026-04-15 02:24:59 +0700

    fix: update title format in Inertia app to use '|' instead of '-'

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 const appName = import.meta.env.VITE_APP_NAME || 'Sollu Indonesia';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} | ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
