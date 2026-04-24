7dad83ea1dcc0f71b094b75a0101f6aa70cbeff3
Wahyu Kristiawan <whykris97@gmail.com>
2026-04-24 15:15:33 +0700

    fix: Reorder sitemap entries for correct priority and change frequency

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -23,12 +23,12 @@ class GenerateSitemap extends Command
             ->setPriority(1.0)
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
 
-        $sitemap->add(Url::create('/services')
-            ->setPriority(0.8)
-            ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY));
-
         $sitemap->add(Url::create('/pricing')
             ->setPriority(1.0)
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+
+        $sitemap->add(Url::create('/services')
+            ->setPriority(0.8)
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY));
 
         $sitemap->add(Url::create('/faq')
